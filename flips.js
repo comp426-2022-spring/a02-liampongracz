d22dae81ee4ff68699f82abb46327f379bf53340
liampong <liam.pongracz@gmail.com>
2022-03-01 15:05:16 -0500

    changed flips.js

--- flips.js
+++ flips.js
@@ -1,8 +1,10 @@
-import { coinFlips } from "./modules/coin.mjs";
+import { coinFlips, countFlips } from "./modules/coin.mjs";
 import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 
 const args = require('minimist')(process.argv.slice(2))
 const flips = args['number'] || 1
-console.log(coinFlips(flips))
+const values = coinFlips(flips)
+console.log(values)
+console.log(countFlips(values))
--- guess-flip.js
+++ guess-flip.js
@@ -4,15 +4,13 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 try{
-const args = require('minimist')(process.argv.slice(2))
+    const args = require('minimist')(process.argv.slice(2))
 
-const call = args['call']
-if (call == null || (call != 'heads' && call != 'tails')) {
-    throw err
-}
-console.log(flipACoin(call))
-
+    const call = args['call']
+    if (call == null || (call != 'heads' && call != 'tails')) {
+        throw err
+    }
+    console.log(flipACoin(call))
 } catch (err) {
-    console.log(`Error: no input.
-    Usage: node guess-flip --call[heads|tails]`)
+    console.log(`Error: no input.\nUsage: node guess-flip --call[heads|tails]`)
 }
